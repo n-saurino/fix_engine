be572a411c39bb623ce9b8b189b798faef00066a
Nigel Saurino <nigelsaurino@gmail.com>
2025-03-20 18:22:27 -0500

    refactor(memory): Redesign Memory Pool with SPSC queue paradigm and compile-time capacity
    Refactored the Memory Pool to align with the principles used in Charles Frasch's Single Producer Single Consumer (SPSC) queue. Introduced templating to define a compile-time capacity, enabling a stack-allocated free list for improved performance and memory efficiency. Reworked helper functions to utilize a stack-style free list, eliminating fragmentation issues present in the previous implementation.
    
    Changes:
    - Refactored the Memory Pool using paradigms from Charles Frasch’s SPSC queue.
    - Templated the Memory Pool and introduced a compile-time capacity for stack allocation.
    - Reworked helper functions to implement a stack-based free list, replacing the previous fragmented array-based approach.
    
    Next steps:
    - Add thread-local support for the Memory Pool to optimize memory management.
    - Integrate an LFQueue to pass FIX message buffer pointers from the application's thread-local memory pool to the TX Network Handler thread.
    - Integrate an LFQueue to return FIX message buffer pointers from the TX Network Handler thread for deallocation in the application’s thread-local memory pool.
    
    Authored and reviewed by: Nigel Saurino

--- include/MemoryPool.hpp
+++ include/MemoryPool.hpp
@@ -2,77 +2,96 @@
 #include <cstddef>
 #include <cstring>
 #include <iostream>
+#include <memory>
+#include <new>
+#include <vector>
 
 #include "Macros.hpp"
 
-class MemoryPool {
+/* Refactoring to make templated and integrate some things I learned from
+ * Charles Frasch's SPSC LFQueue implementation.*/
+
+// this MemoryPool is intended to be instantiated with a char[512] buffer for T.
+// opted to template the Memory Pool on Capacity to make it a compile time
+// value and allow us to have a stack allocated free_list_ instead of using
+// a preallocated std::vector with reserve() that requires indirection to the
+// heap
+template <typename T, size_t Capacity, typename Alloc = std::allocator<T>>
+class MemoryPool : private Alloc {
  public:
-  MemoryPool() : curr_idx_{0} {
-    // initialize the array storing the flags for which blocks are free
+  using ValueType = T;
+  using allocator_traits = std::allocator_traits<Alloc>;
+  using SizeType = typename allocator_traits::size_type;
+
+  explicit MemoryPool(Alloc const& alloc = Alloc{})
+      : Alloc{alloc},
+        kTotalBlocks{Capacity},
+        kBlockSize{sizeof(T)},
+        kMemPoolCapacity{kTotalBlocks * kBlockSize},
+        curr_idx_{Capacity - 1},
+        pool_{allocator_traits::allocate(*this, Capacity)} {
+    ASSERT((Capacity & (Capacity - 1)) == 0,
+           "MemoryPool total blocks must be a power of 2...");
+    ASSERT((kBlockSize & (kBlockSize - 1)) == 0,
+           "Template type T size must be a power of 2...");
+
+    // initialize free_list_ stack
     for (int i{}; i < kTotalBlocks; ++i) {
-      free_block_arr_[i] = false;
+      free_list_[i] = i;
     }
 
     // initialize memory pool
     memset(pool_, '\0', kMemPoolCapacity);
   }
 
-  //   ~MemoryPool() = delete;
+  ~MemoryPool() { allocator_traits::deallocate(*this, pool_, kTotalBlocks); }
   //   MemoryPool(const MemoryPool&) = delete;
   //   MemoryPool(MemoryPool&&) = delete;
   //   MemoryPool& operator=(const MemoryPool&) = delete;
   //   MemoryPool& operator=(MemoryPool&&) = delete;
 
-  char* Allocate() {
+  T* Allocate() {
     ASSERT(
         !Full(),
         "Memory pool out of space! Allocation of new memory block failed...\n");
 
-    size_t buffer_idx{curr_idx_};
-    // set the block to 1 in free_bloc_arr_
-    free_block_arr_[curr_idx_++] = true;
-    UpdateNextFreeIdx();
-    // return memory address of the first char of the block
-    return &pool_[buffer_idx * kBlockSize];
+    // return memory address of T block
+    size_t pool_idx = free_list_[curr_idx_--];
+    return &pool_[pool_idx];
   }
 
-  bool Free(const char* buffer_ptr) {
-    if (!buffer_ptr) {
-      return false;
-    }
+  bool Free(const T* buffer_ptr) {
+    ASSERT(buffer_ptr, "Nullptr passed to Free()...");
 
     // intentionally ignoring ptr alignment with block size for speed
-    size_t idx = (&(*buffer_ptr) - pool_) / kBlockSize;
+    size_t idx = buffer_ptr - pool_;
 
     ASSERT(idx < kTotalBlocks,
            "Tried to free memory that is outside of the Memory Pool! Please "
            "look into the pointer that you are passing to Free().\n");
 
-    // set the block to 0 in free_block_arr_
-    free_block_arr_[idx] = 0;
+    ASSERT(!Empty(), "Trying to Free() an empty Memory Pool...");
 
-    if (idx < curr_idx_) {
-      curr_idx_ = idx;
-    }
+    free_list_[curr_idx_++] = idx;
+
     return true;
   }
 
-  auto Full() -> const bool { return curr_idx_ == kTotalBlocks; }
+  auto Empty() -> const bool { return curr_idx_ == kTotalBlocks - 1; };
+  auto Full() -> const bool { return curr_idx_ == 0; }
   auto Size() -> const size_t { return kTotalBlocks; }
+  void Reset() {
+    for (int i{}; i < kTotalBlocks; ++i) {
+      free_list_[i] = i;
+    }
+    curr_idx_ = kTotalBlocks - 1;
+  }
 
  private:
-  static constexpr inline size_t kTotalBlocks{10};
-  static constexpr inline size_t kBlockSize{512};
-  static constexpr inline size_t kMemPoolCapacity{kTotalBlocks * kBlockSize};
-  char pool_[kMemPoolCapacity];
-  // do we need a fast lookup for free blocks? Should it be faster than linear?
-  // linear on small arrays is extremely fast
-  bool free_block_arr_[kTotalBlocks];
-  size_t curr_idx_{0};
-
-  void UpdateNextFreeIdx() {
-    while (curr_idx_ < kTotalBlocks && free_block_arr_[curr_idx_]) {
-      ++curr_idx_;
-    }
-  }
+  size_t kTotalBlocks{};
+  size_t kBlockSize{};
+  size_t kMemPoolCapacity{};
+  T* pool_;
+  SizeType free_list_[Capacity];
+  SizeType curr_idx_{};
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,18 @@
 void RunFIXEngine() { FIXEngine fix_engine{}; }
 
 int main(int argc, char** argv) {
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  // std::this_thread::sleep_for(std::chrono::seconds(2));
 
-  std::thread client_thread{RunFIXEngine};
-  client_thread.join();
+  // std::thread client_thread{RunFIXEngine};
+  // client_thread.join();
+
+  using CharBuffer = char[512];
+  constexpr size_t kNumberBlocks{1024};
+  MemoryPool<CharBuffer, kNumberBlocks> pool{};
+  for (int i{}; i < 10000; ++i) {
+    CharBuffer* ptr = pool.Allocate();
+    pool.Free(ptr);
+  }
 
   // build FIX message buffer
   // FIXMessageBuilder msg_bldr{};
