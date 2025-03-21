e6e623f88778532b39e0ce10bb8ec373b588b370
Nigel Saurino <nigelsaurino@gmail.com>
2025-03-21 12:09:11 -0500

    Restarting and rebuilding docker project

--- include/Macros.hpp
+++ include/Macros.hpp
@@ -8,11 +8,17 @@
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
 inline auto ASSERT(bool condition, const std::string& msg) noexcept {
+#ifndef NDEBUG
   if (UNLIKELY(!condition)) {
     std::cerr << msg << std::endl;
     // throw std::runtime_error(msg);
     exit(EXIT_FAILURE);
   }
+#else
+  // in release mode, do nothing
+  (void)condition;
+  (void)msg;
+#endif
 }
 
 inline auto FATAL(const std::string& msg) noexcept {
